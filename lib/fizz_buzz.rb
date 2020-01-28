65343d84371ab442f2053e92a88195270d02c674
Philip Gaunitz <fluffen@pgaunitz.se>
2020-01-28 15:28:25 +0100

    adding tests and made fizzbuzz app

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,12 @@
+def fizz_buzz(number)
+    if number % 3 == 0 && number % 5 == 0
+        'fizzbuzz'
+    elsif number % 3 == 0
+        'fizz'
+    elsif number % 5 == 0
+        'buzz'
+    else 
+        number
+    end
+
+  end
